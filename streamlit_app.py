6f073230d4ae7b826cfe1a7b219b13711d6f33c6
JennyPenaloza <jennypenaloza97@gmail.com>
2024-09-18 19:40:19 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,10 +10,9 @@ st.write(
     """
 )
 
-toggle_sidebar = st.checkbox("Show Sidebar")
+st.sidebar()
 
-if toggle_sidebar:
-    st.sidebar.title("Sidebar")
+
 # Load the data from a CSV. We're caching this so it doesn't reload every time the app
 # reruns (e.g. if the user interacts with the widgets).
 @st.cache_data
