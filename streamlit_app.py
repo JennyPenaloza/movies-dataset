43790be07d67b16897587fd8984cf7ef605d8e8d
JennyPenaloza <jennypenaloza97@gmail.com>
2024-09-19 16:47:05 -0500

    10

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,8 +63,8 @@ if display:
     if init_data is not None:
 
         figure = plt.figure()
-        figure.set_figwidth(2)
-        figure.set_figheight(2)
+        figure.set_figwidth(10)
+        figure.set_figheight(10)
         axes = figure.add_subplot(1, 1, 1)
 
         pixels = np.array([255 - p * 255 for p in init_data], dtype='uint8')
